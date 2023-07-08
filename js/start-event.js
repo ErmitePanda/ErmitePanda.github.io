0add467a2b3f1b831391c70fe1253411ddc062ac
Théo Dubernet <95922914+ErmitePanda@users.noreply.github.com>
2023-07-08 21:00:00 +0200

    Actualisation des événements + ajout d'un event à gérer plus tard pour le responsive

--- js/start-event.js
+++ js/start-event.js
@@ -29,14 +29,15 @@ const boxUser = document.querySelector(".box-user")
 let startShowFrame;
 let duration = 700;
 const rectUserBox = boxUser.getBoundingClientRect()
-const posEndAnimeUserBox = rectUserBox.top - 200
+const posEndAnimeUserBox = rectUserBox.top - 200;
 
 function animeUserPosition(tempsCourant) {
     // temps de départ de lanimation (mili seconde tjr)
     if (!startShowFrame) startShowFrame = tempsCourant;
 
     const tempsEcoule = tempsCourant - startShowFrame;
-    const progression = Math.min(tempsEcoule / duration, 0.8);
+    const progression = Math.min(tempsEcoule / duration, 1);
+
     // Interpolation
     const positionCourante = Math.round(rectUserBox.top + (posEndAnimeUserBox - rectUserBox.top) * progression);
 
@@ -51,9 +52,62 @@ function animeUserPosition(tempsCourant) {
 }
 
 /**
+ * Box quizz qui apparaît quand on atteint un seuil de l'écran
+ */
+
+const boxQuizz = document.querySelector(".box-quizz")
+
+function appearQuizzBox() {
+    let startTimeurAnim = null;
+    let dureeApparition = 1000;
+
+    function animeQuizzBox(tempsCourant) {
+        if (!startTimeurAnim) startTimeurAnim = tempsCourant;
+
+        const tempsEcoulee = tempsCourant - startTimeurAnim;
+        const progression = Math.min(tempsEcoulee/dureeApparition, 1)
+
+        boxQuizz.style.opacity = progression.toString();
+        boxQuizz.style.transform = `scale(${progression})`;
+
+        if (progression < duration) {
+            requestAnimationFrame(animeQuizzBox);
+        }
+    }
+
+    requestAnimationFrame(animeQuizzBox);
+}
+
+function scrollQuizzDetection() {
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+
+    if (scrollPosition >= 60) {
+        appearQuizzBox();
+        window.removeEventListener('scroll', scrollQuizzDetection);
+    }
+}
+
+/**
+ * Function qui quand on arrive sur la page initialise le scroll et
+ * ensuite ajoute les différents événement de la page
+ */
+
+function scrollInit() {
+    window.scrollTo(0,0);
+    window.removeEventListener("scroll", scrollInit);
+    initEvent();
+}
+
+function initEvent() {
+    window.addEventListener('scroll', scrollQuizzDetection);
+}
+
+/**
  * MAIN
  */
 
 animationUtilisateur()
 // Démarre l'animation
 requestAnimationFrame(animeUserPosition)
+window.addEventListener('scroll', scrollInit)
