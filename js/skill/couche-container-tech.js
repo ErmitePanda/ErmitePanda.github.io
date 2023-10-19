a5a5650d73769265b6329e8335219a3c18f48af4
Théo Dubernet <95922914+ErmitePanda@users.noreply.github.com>
2023-10-19 14:34:06 +0200

    Réinitialisation du projet

--- js/skill/couche-container-tech.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const containerTech = document.querySelector('.container-tech');
-    const children = containerTech.querySelectorAll('div');
-    let zIndex = 0;
-    children.forEach(child => {
-        child.style.zIndex = zIndex.toString();
-        zIndex++;
-    });
-})
