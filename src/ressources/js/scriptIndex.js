0f131d023a5a46ad92efbd5ce27c39976d7bef61
Théo Dubernet <95922914+ErmitePanda@users.noreply.github.com>
2024-02-23 02:41:00 +0100

    Ajout de l'effet sur le langage du site

--- src/ressources/js/scriptIndex.js
+++ src/ressources/js/scriptIndex.js
@@ -1,6 +1,7 @@
 //////////////////// ELEMENT RECUPERER //////////////////////
 const easterEgg = document.getElementById("easter-egg");
-
+const imgFr = document.getElementById("vf");
+const imgEng = document.getElementById("ve");
 
 /////////////////// Définition de la couleur random //////////////////////
 
@@ -19,6 +20,30 @@ function randomColor(element) {
     element.classList.add('randomColor');
 }
 
+/**
+ * Fonction associer à un event listener de click sur les deux images (imgFr et imgEng)
+ */
+
+function changementDeLangue() {
+    console.log("Changement de langue");
+    if (imgFr.classList.contains('visible')) {
+        imgFr.classList.remove('visible');
+        imgFr.classList.add('cachee');
+        imgEng.classList.remove('inexistant');
+        imgEng.classList.remove('cachee');
+        imgEng.classList.add('visible');
+    } else {
+        imgEng.classList.remove('visible');
+        imgEng.classList.add('cachee');
+        imgEng.classList.add('inexistant');
+        imgFr.classList.remove('inexistant');
+        imgFr.classList.remove('cachee');
+        imgFr.classList.add('visible');
+    }
+}
+
 ////////////////////// MAIN /////////////////////////////////
 
 randomColor(easterEgg);
+imgFr.addEventListener('click', changementDeLangue);
+imgEng.addEventListener('click', changementDeLangue);
